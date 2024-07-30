8763d554631ba87d963f9dad789af55c444a222b
Felix Wick <felix.wick@gmx.net>
2024-07-30 19:46:35 +0200

    cleanup

--- examples/yolo_sam.py
+++ examples/yolo_sam.py
@@ -45,11 +45,11 @@ def draw_bounding_boxes(image, result):
     cv2.imwrite("example_with_bounding_boxes.jpg", image)
 
 
-def draw_mask(image, mask_generated) :
-  masked_image = image.copy()
-  masked_image = np.where(mask_generated.astype(int), np.array([0,255,0], dtype='uint8'), masked_image)
-  masked_image = masked_image.astype(np.uint8)
-  return cv2.addWeighted(image, 0.3, masked_image, 0.7, 0)
+def draw_mask(image, mask_generated):
+    masked_image = image.copy()
+    masked_image = np.where(mask_generated.astype(int), np.array([0,255,0], dtype='uint8'), masked_image)
+    masked_image = masked_image.astype(np.uint8)
+    return cv2.addWeighted(image, 0.3, masked_image, 0.7, 0)
 
 
 def main(args):
